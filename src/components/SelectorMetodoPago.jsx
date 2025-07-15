60bcf3d4902f96c01e19bf3e1a2e27364f2746a2
CMOLERO20 <molero.clara@gmail.com>
2025-07-15 19:55:22 -0300

    intwnto 3

--- src/components/SelectorMetodoPago.jsx
+++ src/components/SelectorMetodoPago.jsx
@@ -13,7 +13,7 @@ export default function SelectorMetodoPago({ onMetodoSeleccionado }) {
 
       <select
         className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        value={metodo}
+        value={''}
         onChange={(e) => setMetodo(e.target.value)}
        
       >
